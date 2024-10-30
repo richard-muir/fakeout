bd3f1d7bf2c0618908e1345b9f8a47a40bfdae47
richard-muir <richard.f.muir@gmail.com>
2024-10-30 22:54:29 +0200

    Add docstrings & types

--- src/data_generator.py
+++ src/data_generator.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import random
 import pytz
+from typing import List, Dict, Any, Iterator
 
 from config import Config
 
@@ -11,7 +12,7 @@ class DataGenerator:
     Generates data based on the passed Config object
     """
     
-    def __init__(self, config):
+    def __init__(self, config: Config) -> None:
         self.data_description = config.data_description
         self.keep_on_swimming = True
         self.datatype_lookup = {
@@ -19,35 +20,48 @@ class DataGenerator:
             'numeric' : self._generate_numeric_data
         }
 
-    def generate(self):
+    def generate(self) -> Iterator[Dict[str, Any]]:
+        """Generate fake data continuously until stopped."""
         while self.keep_on_swimming:
             yield self._generate_fake_data()
 
-    def stop(self):
+    def stop(self) -> None:
+        """Stop the data generation process."""
         self.keep_on_swimming = False  # Method to set the flag to stop
 
-    def _generate_fake_data(self):
-        timestamp_data = [self._generate_datetime_data()]
-        other_data = []
-        for datapoint in self.data_description:
-            data_type = datapoint['data_type']
-            name = datapoint['name']
-            allowable_values = datapoint['allowable_values']
-            generating_fn = self.datatype_lookup[data_type]
-            other_data.append(generating_fn(name, allowable_values))
-                              
-        all_data = timestamp_data + other_data 
+    def _generate_fake_data(self) -> Dict[str, Any]:
+        """Generate a complete set of fake data including a timestamp and other
+        fields defined in the config."""
+        all_data = self._generate_datetime_data()
         
+        for datapoint in self.data_description:
+            try:
+                data_type = datapoint['data_type']
+                name = datapoint['name']
+                allowable_values = datapoint['allowable_values']
+                generating_fn = self.datatype_lookup[data_type]
+                all_data.update(generating_fn(name, allowable_values))
+            except Exception as e:
+                print(f"Error generating data for {name}: {e}")
+                continue  # Skip this data poin
+                              
         return all_data
 
-    def _generate_datetime_data(self):
+    def _generate_datetime_data(self) -> str:
+        """Generate the current UTC datetime."""
         now = datetime.now(pytz.utc)
         return {'datetime': now.isoformat()}
     
-    def _generate_categorical_data(self, name, choices):
+    def _generate_categorical_data(
+            self, 
+            name: str, 
+            choices: List[str]
+            ) -> Dict[str, str]:
+        """Generate categorical data based on the provided choices."""
         return {name : random.choice(choices)}
     
-    def _generate_numeric_data(self, name, data_range):
+    def _generate_numeric_data(self, name: str, data_range: List[float]) -> Dict[str, float]:
+        """Generate numeric data based on the provided range."""
         return {name : random.uniform(data_range[0], data_range[1])}
 
 
