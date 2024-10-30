4af9912b4324e6f6d6912336cca1ff57dd30e2d3
richard-muir <richard.f.muir@gmail.com>
2024-10-31 00:29:42 +0200

    Create a toy streaming service

--- src/streaming_service.py
+++ src/streaming_service.py
@@ -1,4 +1,17 @@
 
 
-class StreamingSercie:
-    pass
+class StreamingService:
+    
+    
+    def __init__(self, config, batch_path='../public'):
+        """
+        Initializes the StreamingService with configuration and setup paths for export.
+        
+        Args:
+            config (dict): Configuration dictionary for batch settings.
+            batch_path (str): Directory path for saving batch files. Defaults to '../public'.
+        """
+        self.interval = config['streaming']['interval']
+
+    def push(self, data):
+        print(data)
