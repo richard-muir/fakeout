a3a78166c7d2c6c5257991e3b8617cfb09ad8fe1
richard-muir <richard.f.muir@gmail.com>
2024-11-05 12:18:30 +0200

    First pass at BatchService controller class

--- src/batch_service.py
+++ src/batch_service.py
@@ -4,6 +4,9 @@ from datetime import datetime, timedelta
 import pytz
 from typing import List, Dict, Optional
 
+from batch_event_handlers import *
+from data_generator import DataGenerator
+
 class BatchService:
     """
     Manages batch data exports and cleans up old exports based on a specified interval.
@@ -15,8 +18,13 @@ class BatchService:
         path (str): Complete path for the batch file, including the filename stem.
         data (list): List of data entries to be exported in each batch.
     """
+    EVENT_HANDLER_LOOKUP = {
+        'local' : LocalStorageConnection,
+        'google_cloud_storage' : GoogleCloudStorageConnection
+        # Add other handlers here as needed
+    }
 
-    def __init__(self, config: Dict[str, str], batch_path: str = 'public') -> None:
+    def __init__(self, config: Dict, batch_path: str = 'public') -> None:
         """
         Initializes the BatchService with configuration and setup paths for export.
         
@@ -24,16 +32,45 @@ class BatchService:
             config (dict): Configuration dictionary for batch settings.
             batch_path (str): Directory path for saving batch files. Defaults to '../public'.
         """
+        self.n_records_pushed = 0
+
+        self.service_name = config.name
+        self.interval = config.interval
+        self.block_size = config.size
+        self.randomise = config.randomise
+        self.filetype = config.filetype
+        self.cleanup_after = config.cleanup_after
+        self.connection_details = config.connection
+        self.data_description = config.data_description
+
+        self.data_generator = DataGenerator(
+            self.data_description, 
+            config.datetime_format_string
+            )
         self.datetime_format_string = config.datetime_format_string
-        self.interval = config.batch_interval
-        self.filename_stem = config.batch_file_name
-        self.batch_path = batch_path
-        self.cleanup_after = config.batch_cleanup_after
-        self.path = os.path.join(batch_path, self.filename_stem)
-        self.data = []
+
+        # Overwrite this from the config to place all the files in the public directory
+        self.batch_data_destination = os.path.join(batch_path, config.connection['folder_path'])
+        config.connection['folder_path'] = self.batch_data_destination
+
+        self.connect_to = self.connection_details['service']
+
+        # Validate service type, create event handler and connect
+        if self.connect_to not in self.EVENT_HANDLER_LOOKUP:
+            raise ValueError(f"Service '{self.connect_to}' is not supported.")
+        
+        self.event_handler = self.EVENT_HANDLER_LOOKUP[self.connect_to](self.connection_details)
+        self.event_handler.connect()    
+
+
+    def generate(self) -> List:
+        """
+        Generates a chunk of data
+        """
+        return self.data_generator.generate(num_records=self.block_size)
         
 
-    def export_batch(self) -> None:
+    def export_batch(self, data: List) -> None:
         """
         Exports the current batch of data to a JSON file, appending a timestamp to the filename.
         
@@ -41,9 +78,9 @@ class BatchService:
         is derived from `self.filename_stem` with the current UTC timestamp appended.
         """
         now = datetime.now(pytz.utc).strftime(self.datetime_format_string)
-        output_path = self.path + f'_{now}.json'
-        with open(output_path, 'w') as f:
-            json.dump(self.data, f, indent=4)
+        filename = self.service_name + f'_{now}.{self.filetype}'
+
+        self.event_handler.export(data, filename)
         
 
     def clean_old_exports(self) -> None:
@@ -56,36 +93,4 @@ class BatchService:
         now = datetime.now(pytz.utc)
         cutoff_time = now - timedelta(seconds=self.cleanup_after)
 
-        deleted_files = []
-        # Iterate through files in the folder
-        for filename in os.listdir(self.batch_path):
-            if filename.startswith(self.filename_stem):  # Check if it's an export file
-                file_time = self.__parse_timestamp_from_filename(filename)
-                
-                # Check if the file is older than the cutoff time
-                if file_time and file_time < cutoff_time:
-                    self.__delete_file(filename)
-                    deleted_files.append(filename)
-        print(f"{len(deleted_files)} exported batch files deleted")
-
-
-    def _clear_batch_data(self) -> None:
-        """Resets the batch data to an empty list"""
-        self.data = []
-
-
-    def __parse_timestamp_from_filename(self, filename: str) -> Optional[datetime]:
-        """Extracts and returns the datetime object from the filename."""
-        try:
-            timestamp_str = filename.split('_')[-1].replace('.json', '')
-            return datetime.strptime(timestamp_str, self.datetime_format_string).astimezone(pytz.utc)
-        except ValueError:
-            print(f"Could not parse timestamp from filename: {filename}")
-            return None
-        
-
-    def __delete_file(self, filename: str) -> None:
-        """Deletes the specified file and logs the deletion."""
-        file_path = os.path.join(self.batch_path, filename)
-        os.remove(file_path)
-        print(f"Deleted old export file: {file_path}")
+        self.event_handler.clean_old_exports(cutoff_time, self.service_name)
