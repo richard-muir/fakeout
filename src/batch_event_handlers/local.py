fe23c2c40162d599625c655226f30cf63c5f6b56
richard-muir <richard.f.muir@gmail.com>
2024-11-07 16:56:48 +0200

    Add error handling to local batch export

--- src/batch_event_handlers/local.py
+++ src/batch_event_handlers/local.py
@@ -1,5 +1,7 @@
 import json
 import os
+import sys
+import inspect
 from typing import Any, Dict, Optional
 from datetime import datetime, timedelta
 import pytz
@@ -33,6 +35,15 @@ class LocalStorageConnection(BaseBatchConnection):
         upload(data: Any, destination: str):
             Uploads data to the designated path within the GCS bucket.
     """
+    ALL_POSSIBLE_ERRORS = (
+            PermissionError,
+            OSError,
+            FileNotFoundError, 
+            TypeError,
+            Exception,
+            ValueError, 
+            AttributeError
+            )
 
     def __init__(self, name, connection: Dict[str, Any]) -> None:
         """
@@ -55,8 +66,12 @@ class LocalStorageConnection(BaseBatchConnection):
         Creates the folder if necessary.
 
         """
-        os.makedirs(self.folder_path, exist_ok=True)
-        return True
+        try:
+            os.makedirs(self.folder_path, exist_ok=True)
+            print(f"Connected successfully. Folder '{self.folder_path}' is ready.")
+        except self.ALL_POSSIBLE_ERRORS as e:
+            self._handle_errors(e)
+            sys.exit(1)
 
 
     def export(self, data: Any, filename: str):
@@ -67,8 +82,13 @@ class LocalStorageConnection(BaseBatchConnection):
         is derived from `self.filename_stem` with the current UTC timestamp appended.
         """
         output_path = os.path.join(self.folder_path, filename)
-        with open(output_path, 'w') as f:
-            json.dump(data, f, indent=4)
+        try:
+            with open(output_path, 'w') as f:
+                json.dump(data, f, indent=4)
+            print(f"Data successfully exported to {output_path}")
+        except self.ALL_POSSIBLE_ERRORS as e:
+            self._handle_errors(e)
+            sys.exit(1)
 
 
     def clean_old_exports(self, cutoff_time: datetime, filename_prefix: str) -> None:
@@ -99,15 +119,77 @@ class LocalStorageConnection(BaseBatchConnection):
     def __parse_timestamp_from_filename(self, filename: str) -> Optional[datetime]:
         """Extracts and returns the datetime object from the filename."""
         try:
+            if not isinstance(filename, str):
+                raise TypeError(f"Filename must be a string. Received: {type(filename)}")
+
+            # Attempt to split and extract the timestamp part
             timestamp_str = filename.split('_')[-1].replace('.json', '')
+            
+            # Parse the timestamp and convert it to UTC timezone
             return datetime.strptime(timestamp_str, self.datetime_format_string).astimezone(pytz.utc)
-        except ValueError:
-            print(f"Could not parse timestamp from filename: {filename}")
-            return None
+        
+        except self.ALL_POSSIBLE_ERRORS as e:
+            self._handle_errors(e, additional_context=f"Filename: {filename}")
+            sys.exit(1)
         
 
     def __delete_file(self, filename: str) -> None:
         """Deletes the specified file and logs the deletion."""
-        file_path = os.path.join(self.folder, filename)
-        os.remove(file_path)
-        print(f"Deleted old export file: {file_path}")
+        try:
+            file_path = os.path.join(self.folder, filename)
+            os.remove(file_path)
+            print(f"Deleted old export file: {file_path}")
+        except self.ALL_POSSIBLE_ERRORS as e:
+            self._handle_errors(e, additional_context=f"Filename: {filename}")
+            sys.exit(1)
+            
+
+
+    def _handle_errors(self, exception: Exception, additional_context: str=''):
+        """Handles connection and publishing related errors with descriptive messages."""
+
+        # Get the name of the calling function to determine the context
+        calling_function = inspect.stack()[1].function
+        contexts = {
+            'connect' : 'connecting',
+            'export' : 'exporting',
+            'clean_old_exports' : 'cleaning old exports',
+            '__parse_timestamp_from_filename' : 'parsing timestamps from filename',
+            '__delete_file' : 'deleting files'
+        }
+        context = contexts[calling_function]
+
+        # Want to highlight additional context if it's passed
+        if additional_context:
+            additional_context = f"\nAdditional context :{additional_context}"
+
+        if isinstance(exception, PermissionError):
+            error_message = (
+                f"Error during {context} for batch export service '{self.name}': Insufficient permissions to create, write to, or access "
+                f"'{self.folder_path}'. Please check folder permissions.{additional_context}"
+            )
+        elif isinstance(exception, OSError):
+            error_message = (
+                f"Error during {context} for batch export service '{self.name}': Unable to create or access the folder "
+                f"'{self.folder_path}'. Details: {exception}{additional_context}"
+            )
+        elif isinstance(exception, FileNotFoundError):
+            error_message = (
+                f"Error during {context} for batch export '{self.name}': The directory '{self.folder_path}' does not exist.\n"
+                "Please ensure the folder path is valid.{additional_context}"
+            )
+        elif isinstance(exception, TypeError):
+            error_message = (
+                f"Error during {context} for batch export '{self.name}': Failed to serialize data to JSON format.\n"
+                f"Details: {exception}\n"
+                "Ensure the data is JSON serializable.{additional_context}"
+            )
+        else:
+            error_message = (
+                f"Unexpected error during {context} for batch export service {self.name}: An error occurred while accessing "
+                f"'{self.folder_path}'. Details: {exception}{additional_context}"
+            )
+
+        print(error_message)
+
+
